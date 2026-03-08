db7f952165aa68fa9cc4d01def7ced14e23a16a1
Kailash Khadka <k.khadka343@gmail.com>
2026-03-08 06:38:47 -0400

    Unified catkin workspace & temp session configs
    Signed-off-by: Kailash Khadka <k.khadka343@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,17 @@
 set -e
 # Source ROS and the workspace on every container start
 [ -f /opt/ros/${ROS_DISTRO}/setup.bash ] && source /opt/ros/${ROS_DISTRO}/setup.bash
-[ -f /root/catkin_ws/devel/setup.bash ] && source /root/catkin_ws/devel/setup.bash
+[ -f /root/${WS}/devel/setup.bash ] && source /root/${WS}/devel/setup.bash
+
+if [ -f /root/${WS}/session_tmp.yml ]; then
+    cp /root/${WS}/session_tmp.yml /root/${WS}/src/nbvp_exploration/startup/kopterworx_one_flying/session.yml
+fi
+
+if [ -f ~/${WS}/devel/setup.bash ]; then
+    echo "alias ws='source ~/${WS}/devel/setup.bash'" >> ~/.bashrc
+    source ~/${WS}/devel/setup.bash
+fi
+
+source ~/.bashrc
 
 exec "$@"
